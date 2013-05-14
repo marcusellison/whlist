56d1c1a442111428251d9e662be57fc3971d021d
Marcus Ellison <marcus@wurkhappy.com>
2013-05-14 18:14:56 -0400

    hopefully fixing mixpanel recording job clicks

--- static/js/script.js
+++ static/js/script.js
@@ -2,9 +2,6 @@
 
 $(document).ready(function() {
   mixpanel.track("Page Views", { Target:"Technology"});
-
   mixpanel.track_forms("#view_jobs", "Job List Requests", { Location: "NYC"});
-
-  mixpanel.track_links("#job_url", "Job Clicks", { Type:"Technology"});
-
+  mixpanel.track_links(".job_url", "Job Clicks", { Type:"Technology"});
 });
